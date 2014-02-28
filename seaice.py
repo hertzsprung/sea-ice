c1d75924deda266e6ad6ab9fe06ea59d779650bc
katabat <heds_1@hotmail.com>
2014-02-28 11:10:23 +0000

    additional L1 & L2 variables

--- seaice.py
+++ seaice.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 from pyhdf import SD
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime
 
 #File names
 amsr_name = 'AMSR_E_L2A_BrightnessTemperatures_V10_201012141724_D.hdf'
@@ -14,12 +17,20 @@ l2 = SD.SD(callid2_name)
 #Reading Calipso level 1 data
 
 l1utc = l1.select('Profile_UTC_Time')
-tab532 = l1.select('Total_Attenuated_Backscatter_532')
-pab532 = l1.select('Perpendicular_Attenuated_Backscatter_532')
+l1tab532 = l1.select('Total_Attenuated_Backscatter_532')
+l1pab532 = l1.select('Perpendicular_Attenuated_Backscatter_532')
 l1lon = l1.select('Longitude')
 l1lat = l1.select('Latitude')
 
 #Reading Calipso level 2 data
+# Infrared Radiometer (IIR) Data
 
 l2utc = l2.select('Profile_UTC_Time')
-lse = l2.select('Lidar_Surface_Elevation')
+l2lse = l2.select('Lidar_Surface_Elevation')
+
+#Calipso Analysis
+
+
+
+#------------------------------
+#Reading AMSR-E data
