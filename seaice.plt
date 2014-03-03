f4d326bae558e335b6bb1e3bace9800348f19d24
James Shaw <js102@zepler.net>
2014-03-03 12:27:58 +0000

    get ice calculations working (ish)

--- seaice.plt
+++ seaice.plt
@@ -1,15 +1,11 @@
 set term postscript eps enhanced color size 6,4
 
+set datafile missing 'NA'
+
 unset key
 set autoscale
 set view map
-GR(t_b_18_7v, t_b_36_5v) = (t_b_36_5v - t_b_18_7v) / (t_b_36_5v + t_b_18_7v)
-PR(t_b_18_7h, t_b_18_7v) = (t_b_18_7v - t_b_18_7h) / (t_b_18_7v + t_b_18_7h)
 
-set output "pr.eps"
-plot 'amsre.dat' using 7:6:(PR($5, $3)) with points palette pointtype 0, \
- 'world.dat' with lines lt 3
-
-set output "gr.eps"
-plot 'amsre.dat' using 7:6:(GR($4, $3)) with points palette pointtype 0, \
+set output "ice.eps"
+plot 'amsre.dat' using 2:1:3 with points palette pointtype 0, \
  'world.dat' with lines lt 3
--- seaice.py
+++ seaice.py
@@ -5,6 +5,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 import datetime
 
+def gr(t_b_37v, t_b_19v):
+	if (t_b_37v + t_b_19v == 0):
+		return 0.0
+	else:
+		return (t_b_37v - t_b_19v)/(t_b_37v + t_b_19v)
+
+def pr(t_b_19v, t_b_19h):
+	if (t_b_19h + t_b_19v == 0):
+		return 0.0
+	else:
+		return (t_b_19v - t_b_19h)/(t_b_19v + t_b_19h)
+
+def gr_low(pr):
+	return 0.75 * pr - 0.1175
+
+def gr_high(pr):
+	return 0.35 * pr - 0.01175
+
+def ice(pr, gr):
+	if (pr < 0.05 or pr > 0.25):
+		return 'NA'
+	elif (gr >= gr_low(pr) and gr <= gr_high(pr)):
+		return 1.0 - ((pr-0.05) * 5.0)
+	else:
+		return 'NA'
+
 #File names
 amsr_name = 'AMSR_E_L2A_BrightnessTemperatures_V10_201012141724_D.hdf'
 
@@ -17,8 +43,13 @@ t_b_36_5v_grid = amsr.select("36.5V_Res.1_TB").get()
 latitude_grid = amsr.select("Latitude").get()
 longitude_grid = amsr.select("Longitude").get()
 
-print '#x, y, 18_7v, 36_5v, 18_7h, lat, long'
+print '#lat long ice pr gr gr_low gr_high'
 
 for x, t_b_18_7v_list in enumerate(t_b_18_7v_grid):
 	for y, t_b_18_7v in enumerate(t_b_18_7v_list):
-		print x, y, t_b_18_7v, t_b_36_5v_grid[x][y], t_b_18_7h_grid[x][y], latitude_grid[x][y], longitude_grid[x][y]
+		t_b_19v_rescaled = 0.01 * float(t_b_18_7v) + 327.68
+		t_b_19h_rescaled = 0.01 * float(t_b_18_7h_grid[x][y]) + 327.68
+		t_b_37v_rescaled = 0.01 * float(t_b_36_5v_grid[x][y]) + 327.68
+		gr_value = gr(t_b_37v_rescaled, t_b_19v_rescaled)
+		pr_value = pr(t_b_19v_rescaled, t_b_19h_rescaled)
+		print latitude_grid[x][y], longitude_grid[x][y], ice(pr_value, gr_value), pr_value, gr_value, gr_low(pr_value), gr_high(pr_value)
